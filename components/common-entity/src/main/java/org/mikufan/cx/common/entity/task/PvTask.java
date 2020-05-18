e85949f1d6cbd1c8578819f03e91a403eeb94615
CXwudi <1113421658@qq.com>
2020-05-18 16:43:44 -0400

    :recycle: use lombok non null annotation to further improve readability

--- components/common-entity/src/main/java/org/mikufan/cx/common/entity/task/PvTask.java
+++ components/common-entity/src/main/java/org/mikufan/cx/common/entity/task/PvTask.java
@@ -1,33 +1,32 @@
-package org.mikufan.cx.common.entity.task.pv;
+package org.mikufan.cx.common.entity.task;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.eclipse.collections.api.factory.Sets;
 import org.eclipse.collections.api.set.MutableSet;
-import org.mikufan.cx.common.entity.task.Task;
+import org.mikufan.cx.common.entity.task.pv.AbstractPv;
 
 /**
  * a task for manipulating around Pvs
  * @author CX无敌
  */
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class PvTask<P extends AbstractPv> implements Task<P> {
   //we use <P extends AbstractPv> to allow either storing generic AbstractPv or just one subtype of AbstractPv
 
-  @Getter
+  @Getter @NonNull
   @JsonProperty("folderName")
-  protected String folderName = "";
+  protected String folderName;
 
   @JsonProperty("todo")
   protected MutableSet<P> todo = Sets.mutable.empty();
 
   @JsonProperty("done")
   protected MutableSet<P> done = Sets.mutable.empty();
-
-  public PvTask (String folderName) {
-    this.folderName = folderName;
-  }
 
   @Override
   public MutableSet<P> getTodos() {
