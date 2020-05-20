87cb1ae58e3b6c77baf867c8262dca949bb92776
CXwudi <1113421658@qq.com>
2020-05-20 18:16:43 -0400

    :construction_worker: check CI is working

--- components/common-util/src/test/java/org/mikufan/cx/common/util/io/JsonPojoTranslatorTest.java
+++ components/common-util/src/test/java/org/mikufan/cx/common/util/io/JsonPojoTranslatorTest.java
@@ -2,13 +2,18 @@ package org.mikufan.cx.common.util.io;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class JsonPojoTranslatorTest {
   //TODO: complete this test
   @Test
   void testRead() {
+    assertTrue(true);
   }
 
   @Test
   void testWrite() {
+    assertTrue(true);
+
   }
 }
