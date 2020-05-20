8721ad19dab509db79f8504645a6a39d01aa6c8d
CXwudi <1113421658@qq.com>
2020-05-20 17:06:39 -0400

    :bento: Adding implementation of PojoFileTranslator, which is JsonPojoTranslator

---
+++ components/common-util/src/main/java/org/mikufan/cx/common/util/io/PojoFileTranslator.java
@@ -0,0 +1,28 @@
+package org.mikufan.cx.common.util.io;
+
+import java.io.File;
+import java.util.Optional;
+
+/**
+ *
+ * @param <P>
+ * @author CX无敌
+ */
+public interface PojoFileTranslator<P> {
+
+  /**
+   * read a file and return the content in pojo
+   * @param file the file to read
+   * @return the pojo
+   */
+  Optional<P> read(File file);
+
+  /**
+   * write a pojo back to a file
+   * @param pojo the pojo to write
+   * @param dest the file where to write the pojo
+   * @return {@code true} if success
+   */
+  boolean write(P pojo, File dest);
+
+}
---
+++ components/common-util/src/test/java/org/mikufan/cx/common/util/io/JsonPojoTranslatorTest.java
@@ -0,0 +1,14 @@
+package org.mikufan.cx.common.util.io;
+
+import org.junit.jupiter.api.Test;
+
+class JsonPojoTranslatorTest {
+  //TODO: complete this test
+  @Test
+  void testRead() {
+  }
+
+  @Test
+  void testWrite() {
+  }
+}
