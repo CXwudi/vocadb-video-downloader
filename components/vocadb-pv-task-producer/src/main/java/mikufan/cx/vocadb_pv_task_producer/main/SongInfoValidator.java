e24986ada3ce2714cd6288dc35e5760efa01a130
CXwudi <cx2010017@gmail.com>
2020-07-29 22:37:10 -0400

    :egg: Adding SongInfoValidator and need to complete test

---
+++ components/vocadb-pv-task-producer/src/main/java/mikufan/cx/vocadb_pv_task_producer/main/SongInfoValidator.java
@@ -0,0 +1,54 @@
+package mikufan.cx.vocadb_pv_task_producer.main;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mikufan.cx.common_vocaloid_entity.vocadb.models.PVContract;
+import mikufan.cx.common_vocaloid_entity.vocadb.models.SongForApiContract;
+import mikufan.cx.project_vd_common_util.pv_service.SupportedPvServices;
+
+import java.util.Optional;
+
+/**
+ * A stateless validator to check if a song is downloadable
+ * @author CX无敌
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class SongInfoValidator {
+
+  /**
+   * validate the song is downloadable or not base on pv info
+   * @param song the song to be checked
+   * @return optional of string indicate if error message exists or not
+   */
+  public Optional<String> validate(@NonNull SongForApiContract song){
+    // get the list of PVs of a song
+    var pvs = song.getPvs();
+    var name = song.getName();
+
+    //check if no pvs
+    if (pvs == null || pvs.isEmpty()){
+      log.warn("no PVs found in {}", name);
+      return Optional.of("no official PVs found");
+    }
+
+    //check if pvs are inaccessible
+    var accessiblePvs = pvs.reject(PVContract::isDisabled);
+    if (accessiblePvs.isEmpty()){
+      log.warn("all PVs for {} are not accessible on website", name);
+      return Optional.of("all PVs are not accessible from service websites");
+    }
+
+    //check if pvs are not supported
+    var supportedPvs = accessiblePvs.select(pv -> SupportedPvServices.contains(pv.getService()));
+    if (supportedPvs.isEmpty()){
+      log.warn("current {} doesn't contains PVs that are accessible and supported by vocadb-pv-video-downloader", name);
+      return Optional.of("currently we are not able to download PV from web service other than " +
+          SupportedPvServices.getSupportedPvServices().makeString(", "));
+    }
+
+
+    return Optional.empty();
+  }
+}
---
+++ components/vocadb-pv-task-producer/src/test/java/mikufan/cx/vocadb_pv_task_producer/main/SongInfoValidatorTest.java
@@ -0,0 +1,12 @@
+package mikufan.cx.vocadb_pv_task_producer.main;
+
+import org.junit.jupiter.api.Test;
+
+class SongInfoValidatorTest {
+
+  //TODO: complete it
+  @Test
+  void shouldFailIfNoPv() {
+
+  }
+}
