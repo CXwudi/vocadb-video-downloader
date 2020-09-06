30ba8c81e2d3e014889f332ba6135956ad534cd9
CXwudi <cx2010017@gmail.com>
2020-09-06 18:16:31 -0400

    :pencil: enhance doc for SneakyThrow

--- commons/project-vd-common-util/src/main/java/mikufan/cx/project_vd_common_util/exception/SneakyThrow.java
+++ commons/project-vd-common-util/src/main/java/mikufan/cx/project_vd_common_util/exception/SneakyThrow.java
@@ -10,6 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class SneakyThrow {
 
+  /**
+   * sneak throw the exception <br/>
+   * Noticed that you should not use this method to throw an {@link Error}
+   */
   @SuppressWarnings("unchecked")
   public static <T extends Exception, R> R theException(Exception t) throws T {
     throw (T) t; // ( ͡° ͜ʖ ͡°)
