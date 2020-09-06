c2343ab3a54bd4bcaeb56c1c608331c9a5c3e84e
CXwudi <cx2010017@gmail.com>
2020-09-06 17:28:22 -0400

    better description

--- components/vocadb-pv-downloader/src/main/java/mikufan/cx/vocadb_pv_downloader/services/downloaders/root/BasePvDownloader.java
+++ components/vocadb-pv-downloader/src/main/java/mikufan/cx/vocadb_pv_downloader/services/downloaders/root/BasePvDownloader.java
@@ -2,6 +2,7 @@ package mikufan.cx.vocadb_pv_downloader.services.downloaders.root;
 
 
 import mikufan.cx.vocadb_pv_downloader.entity.DownloadStatus;
+import mikufan.cx.vocadb_pv_downloader.entity.DownloadStatusEnum;
 
 import java.nio.file.Path;
 
@@ -12,12 +13,14 @@ import java.nio.file.Path;
 public interface BasePvDownloader {
 
   /**
-   * Download a pv from specific url to specific directory with specific file name
+   * Download a pv from specific url to specific directory with specific file name <br/>
    *
    * <h3>Implementation Note:</h3>
    * The function must not throw exception except for {@link InterruptedException}.
-   * Instead, an exception or a failure must return a {@link DownloadStatus}
-   * with reason/description. Such description can be {@link Throwable#getMessage()}.<br/>
+   * Instead, implementation must always return a {@link DownloadStatus}.
+   * If the result status is not {@link DownloadStatusEnum#SUCCESS},
+   * a description or reason should be set in {@link DownloadStatus#description}.
+   * Such description can be {@link Throwable#getMessage()}.<br/>
    * The implementation must respect that if a download is success,
    * the given directory and fileName in the parameters should able to
    * correctly locate the PV file that is downloaded by this method.
