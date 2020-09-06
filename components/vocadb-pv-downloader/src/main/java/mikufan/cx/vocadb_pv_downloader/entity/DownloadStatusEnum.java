19de9769fe3aa7ba94447d63e92c8e003a3887a0
CXwudi <cx2010017@gmail.com>
2020-09-06 18:24:16 -0400

    :pencil: enhance doc for DownloadStatusEnum

--- components/vocadb-pv-downloader/src/main/java/mikufan/cx/vocadb_pv_downloader/entity/DownloadStatusEnum.java
+++ components/vocadb-pv-downloader/src/main/java/mikufan/cx/vocadb_pv_downloader/entity/DownloadStatusEnum.java
@@ -6,6 +6,16 @@ package mikufan.cx.vocadb_pv_downloader.entity;
  */
 public enum DownloadStatusEnum {
   SUCCESS,
-  FAIL_INITIAL, FAIL_DOWNLOAD, //hard failure
-  UNKNOWN_STATUS; //soft failure, don't know if the download success or not
+  /**
+   * something wrong even before the download process
+   */
+  FAIL_INITIAL,
+  /**
+   * hard failure, something wrong during the download process
+   */
+  FAIL_DOWNLOAD,
+  /**
+   * soft failure, don't know if the download success or not
+   */
+  UNKNOWN_STATUS
 }
