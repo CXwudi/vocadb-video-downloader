5acb1b7b03c21d890bd30f254c0ea7d06a144286
CXwudi <cx2010017@gmail.com>
2020-09-06 21:06:21 -0400

    🃏 added some more Mixinable

---
+++ components/vocadb-pv-downloader/src/main/java/mikufan/cx/vocadb_pv_downloader/services/downloaders/root/annotations/Mixinable.java
@@ -0,0 +1,22 @@
+package mikufan.cx.vocadb_pv_downloader.services.downloaders.root.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Telling that mixin interface can override this method. <br/>
+ * And also contains javadoc explaining the behavior of the mixin interface
+ * @author CX无敌
+ */
+@Target({ElementType.METHOD})
+public @interface Mixinable {
+  /**
+   * explaining how does a mixin interface of this method works
+   */
+  String effect();
+  /**
+   * explain how does the order of multiple mixin interfaces of same type works
+   */
+  String orderEffect() default "order has no affection";
+
+}
---
+++ components/vocadb-pv-downloader/src/main/java/mikufan/cx/vocadb_pv_downloader/services/downloaders/root/mixin/DefaultNullCheckMixin.java
@@ -0,0 +1,26 @@
+package mikufan.cx.vocadb_pv_downloader.services.downloaders.root.mixin;
+
+import mikufan.cx.vocadb_pv_downloader.entity.DownloadStatus;
+import mikufan.cx.vocadb_pv_downloader.entity.DownloadStatusEnum;
+import mikufan.cx.vocadb_pv_downloader.services.downloaders.root.BaseRequestDownloadValidatePvDownloader;
+
+import java.nio.file.Path;
+
+/**
+ * @author CX无敌
+ */
+public interface DefaultNullCheckMixin<Req, Rsp> extends BaseRequestDownloadValidatePvDownloader<Req, Rsp>  {
+  @Override
+
+  default DownloadStatus validateStatus(String url, Path dir, String fileName, Rsp response, Exception exp) {
+    if (exp != null){
+      return new DownloadStatus(DownloadStatusEnum.FAIL_DOWNLOAD,
+          "Exception happens during download " + fileName + " from " + url + " with msg: " + exp.getMessage());
+    } else if (response == null){
+      return new DownloadStatus(DownloadStatusEnum.FAIL_DOWNLOAD,
+          "No response object returned from downloading process during download " + fileName + " from " + url);
+    }
+
+    return new DownloadStatus(DownloadStatusEnum.SUCCESS, "Base check from DefaultNullCheckMixin success");
+  }
+}
