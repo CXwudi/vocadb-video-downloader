0d477a428610cc6a614d911801710627fea3ffda
CXwudi <cx2010017@gmail.com>
2020-12-26 03:41:15 -0500

    revert back to old normaliziaer

--- commons/project-vd-common-util/src/main/java/mikufan/cx/project_vd_common_util/string/FileNameNormalizer.java
+++ commons/project-vd-common-util/src/main/java/mikufan/cx/project_vd_common_util/string/FileNameNormalizer.java
@@ -9,7 +9,7 @@ public class FileNameNormalizer {
 
   public static String removeIllegalChars(String fileName){
     return fileName
-        .replace("/", "_")
+        .replace("/", "-")
         .replace("\\", "-")
 
         .replace("? ", " ")
